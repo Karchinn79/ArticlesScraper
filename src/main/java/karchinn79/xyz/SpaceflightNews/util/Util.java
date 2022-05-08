0aafc9eb57cfa6814acec506801eea5727749646
Karchinn79 <chingizkhankarypov@gmail.com>
2022-05-09 00:00:41 +0300

    el fakko

--- src/main/java/karchinn79/xyz/SpaceflightNews/util/Util.java
+++ src/main/java/karchinn79/xyz/SpaceflightNews/util/Util.java
@@ -5,9 +5,10 @@ import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
-public class util {
-    public static List<String> blackList = new ArrayList<>();
+public class Util {
 
+    public static List<String> blackList = new ArrayList<>();
+    {blackList.add("Intelsat");}
     public  void addWord(String word){
         if(word == null || word.equals("")){
             //exception here
